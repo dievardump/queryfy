7aa4a80bd30f57ca6505692d2320b489f419a2d5
dievardump <simon.fremaux@gmail.com>
2015-09-27 01:26:35 +0200

    initial commit

---
+++ queryfy.js
@@ -0,0 +1,13 @@
+module.exports = function serialize(obj, prefix) {
+    var str = [];
+    for (var p in obj) {
+        if (obj.hasOwnProperty(p)) {
+            var k = prefix ? prefix + "[" + p + "]" : p,
+                v = obj[p];
+            str.push(typeof v == "object" ?
+                serialize(v, k) :
+                encodeURIComponent(k) + "=" + encodeURIComponent(v));
+        }
+    }
+    return str.join("&");
+}
